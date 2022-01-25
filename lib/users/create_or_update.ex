a949216a934fce5e33c099ca013cf948a55b810c
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-01-25 19:45:04 -0300

    feat: create or update user function

---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,20 @@
+defmodule Flightex.Users.CreateOrUpdate do
+
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.User
+
+  def call(%{name: name, email: email, cpf: cpf}) do
+    name
+    |> User.build(email, cpf)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}) do
+    UserAgent.save(user)
+
+    {:ok, user}
+  end
+
+  defp save_user({:error, _user}), do: {:error, "Cpf must be a String"}
+
+end
